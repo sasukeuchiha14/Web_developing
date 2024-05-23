7603a85a079d8eaab7c1a5597de28688c451da6d
sasukeuchiha14 <hgarg7123@gmail.com>
2024-05-24 01:40:17 +0530

    Javascript Loops - forin, forof and while

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
-console.log("Hello I am a conditional tutorial")
-
-let age=17;
-let grace=2;
-
-if ((age+grace)>=18) {
-    console.log("You can drive a car")
+for (let i=0; i<10; i++) {
+  console.log(i)
 }
 
-else if ((age+grace)==17) {
-    console.log("You can drive a car with a guardian")
+let obj = {
+    name: 'John',
+    age: 30,
+    city: 'New York'
 }
 
-else if ((age-grace)==60) {
-    console.log("You are too old to Drive a car")
+
+// forin loop for objects
+for (const key in obj) {
+    console.log(key, obj[key])
 }
 
-else {
-    console.log("You are not allowed to drive a car"); // "; is optional in javascript but it is a good practice to use it to avoid any errors in the code."
+// forof loop for arrays
+for (const c of 'Hello') {
+    console.log(c)
 }
 
-age-=2;
-console.log(age + grace)
-console.log(age - grace)
-console.log(age * grace)
-console.log(age / grace) // / is used for division
-console.log(age % grace) // % is used for modulus
-console.log(age ** grace) // ** is used for exponentiation
+let i=5;
+while (i<10) {
+    console.log(i)
+    i++
+}
 
-console.log("3" == 3) // == is used for comparison
-console.log("3" === 3) // === is used for strict comparison (comp with data type)
-console.log("3" != 3) // != is used for not equal to
-console.log("3" !== 3) // !== is used for not equal to with data type
-
-// && is used for logical and operator
-// || is used for logical or operator
-// ! is used for logical not operator
-
-// Ternary operator
-console.log(age>=18 ? "You can drive a car" : "You are not allowed to drive a car")
+do {
+    console.log(i)
+    i++
+} while (i<20)
