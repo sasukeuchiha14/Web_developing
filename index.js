db2dfee848c950f0c9850e2a21ec5567c3c4ca63
sasukeuchiha14 <hgarg7123@gmail.com>
2024-05-25 09:06:18 +0530

    Javascript Strings

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-let random = Math.random()
-console.log(random)
-let a = prompt("Enter first number")
-let c = prompt("Enter operation")
-let b = prompt("Enter second number")
+console.log("This is String tutorial")
 
-let obj = {
-    "+": "-",
-    "*": "+",
-    "-": "/",
-    "/": "**",
-}
+let a = "Hardik";
+console.log(a[3])
 
-if (random > 0.1) {
-    // Perform correct calculation
-    console.log(`The result is ${a} ${c} ${b}`)
-    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
-}
+console.log(a.length)
 
-else {
-    // Perform wrong calculation
-    c = obj[c]
-    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
-}
+let real_name = "Hardik";
+let friend = "Akshat";
+console.log("His name is " + real_name + " and his friend name is " + friend)
+console.log(`His name is ${real_name} and his friend name is ${friend}`) // We have to use ` (backquotes) ` for this
+
+let b = "Shivam"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+
+console.log(b.indexOf('v'))
+console.log(b.slice(1, 4))
+console.log(b.slice(1))
+
+console.log(b.replace('Shi', 'Hii')) // If 2 occurrences are there then it will replace the first one ONLY
+console.log(a.concat(b, " is a good person", "!!!"))
+
+// All these will not change the original string
+// It will return the new string with the changes made in it
+
+console.log(a.startsWith('Hard'))
+console.log(a.endsWith('dik'))
+console.log(friend.endsWith('mat'))
